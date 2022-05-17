9211b0ff56e4b5bcf15c7b9753f0a688b21f8e83
Jason Thorpe <thorpej@me.com>
2022-05-17 06:43:15 -0700

    Put __format__ attribute on the tla_printf() prototype.

--- LogicAnalyzer/tla.h
+++ LogicAnalyzer/tla.h
@@ -46,7 +46,7 @@ extern "C" {
 
 extern cpu_t cpu;
 
-int tla_printf(const char *, ...);
+int tla_printf(const char *, ...) __attribute__((__format__(__printf__, 1, 2)));
 
 #if defined(__cplusplus)
 }
